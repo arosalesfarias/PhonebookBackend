aed7f7605590b71b5795a92c58a0ed5d36902653
arosalesfarias <arosalesfarias@gmail.com>
2024-03-13 13:01:47 -0300

    Part 3.2, phonebook backend step 2

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get('/', (request, response) =>
     response.send('<h1>Hello World!</h1>')
 )
 
+app.get('/info', (request, response) =>{
+    const length = persons.length
+    const now = new Date()
+    response.send(`<p>Phonebook has info for ${length} people</p><p>${now}</p>`)
+})
+
 app.get('/api/persons', (request, response) =>
     response.json(persons)
 )
