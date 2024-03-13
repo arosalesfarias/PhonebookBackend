b93276b51b637e257fc7cad759b09387bd4ff1c2
arosalesfarias <arosalesfarias@gmail.com>
2024-03-13 13:24:37 -0300

    Part 3.3, phonebook backend step 3

--- index.js
+++ index.js
@@ -38,5 +38,13 @@ app.get('/api/persons', (request, response) =>
     response.json(persons)
 )
 
+app.get('/api/persons/:id', (request, response) =>{
+    const id = Number(request.params.id)
+    const person = persons.find( p => p.id === id)
+    person?
+        response.json(person)
+        :response.status(404).json({error:`Person ${id} not found`})
+})
+
 const PORT = 3001
 app.listen( PORT, () => console.log(`Server running on port ${PORT}`))
