b6c54f120b4aeea6ac68f8307e971da057f4d869
Ariel Rosales <arosalesfarias@gmail.com>
2024-03-13 15:59:13 -0300

    Part3.6, phonebook backend step 6

--- index.js
+++ index.js
@@ -3,48 +3,48 @@ const app = express()
 app.use(express.json())
 
 let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ]
 
 app.get('/', (request, response) =>
-    response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
 )
 
-app.get('/info', (request, response) =>{
-    const length = persons.length
-    const now = new Date()
-    response.send(`<p>Phonebook has info for ${length} people</p><p>${now}</p>`)
+app.get('/info', (request, response) => {
+  const length = persons.length
+  const now = new Date()
+  response.send(`<p>Phonebook has info for ${length} people</p><p>${now}</p>`)
 })
 
 app.get('/api/persons', (request, response) =>
-    response.json(persons)
+  response.json(persons)
 )
 
-app.get('/api/persons/:id', (request, response) =>{
-    const id = Number(request.params.id)
-    const person = persons.find( p => p.id === id)
-    person?
-        response.json(person)
-        :response.status(404).json({error:`Person ${id} not found`})
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  person ?
+    response.json(person)
+    : response.status(404).json({ error: `Person ${id} not found` })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -64,12 +64,17 @@ app.post('/api/persons', (request, response) => {
   const body = request.body
   const person = {
     id: generateId(),
-    name: body.name
+    name: body.name,
+    number: body.number
   }
-  persons = persons.concat(person)
+  if (!person.name || !person.number) return response.status(400).json({ error: 'Name or number missing' })
 
+  const duplicate = persons.find( p => p.name.toLocaleLowerCase() === person.name.toLocaleLowerCase())
+  if (duplicate) return response.status(400).json({ error: `Person ${person.name} exists` })
+
+  persons = persons.concat(person)
   response.json(person)
 })
 
 const PORT = 3001
-app.listen( PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
