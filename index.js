30a0be144310d91dc2f29735fa72596c46f15e18
Ariel Rosales <arosalesfarias@gmail.com>
2024-04-15 13:33:54 -0300

    Part 3.17, phonebook database step 5. HTTP put.

--- index.js
+++ index.js
@@ -91,6 +91,17 @@ app.post('/api/persons', (request, response, next) => {
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatePerson => response.json(updatePerson))
+    .catch(error => next(error))
+})
+
 const errorHandler = (error, request, response, next) => {
   console.log(error.message)
 
