710e8cf8c42c6da079f6890dc55483534c7f078b
Ariel Rosales <arosalesfarias@gmail.com>
2024-04-14 23:20:27 -0300

    Part 3.16, phonebook database step 4. Error Handler middleware

--- index.js
+++ index.js
@@ -54,10 +54,10 @@ app.get('/info', (request, response) => {
   response.send(`<p>Phonebook has info for ${length} people</p><p>${now}</p>`)
 })
 
-app.get('/api/persons', (request, response) =>
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+app.get('/api/persons', (request, response, next) =>
+  Person.find({})
+    .then(persons => response.json(persons))
+    .catch(error => next(error))
 )
 
 app.get('/api/persons/:id', (request, response) => {
@@ -68,13 +68,13 @@ app.get('/api/persons/:id', (request, response) => {
     : response.status(404).json({ error: `Person ${id} not found` })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => response.status(204).end())
-    .catch(error => console.log(error))
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   const person = new Person({
     name: body.name,
@@ -86,8 +86,22 @@ app.post('/api/persons', (request, response) => {
   // if (duplicate) return response.status(400).json({ error: `Person ${person.name} exists` })
 
   persons = persons.concat(person)
-  person.save().then(savedPerson => response.json(savedPerson))
+  person.save()
+    .then(savedPerson => response.json(savedPerson))
+    .catch(error => next(error))
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
