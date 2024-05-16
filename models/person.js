10fe892f03c6f426335ebd82806988660ef555d2
Ariel Rosales <arosalesfarias@gmail.com>
2024-05-16 00:23:02 -0300

    Part 3.20, phonebook database step 8. Person number validation

--- models/person.js
+++ models/person.js
@@ -18,7 +18,14 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true
   },
-  number: String,
+  number: {
+    type: String,
+    validate : {
+      validator: value => /\d{2,3}-\d{4}/.test(value),
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'Person phone number required']
+  }
 })
 
 personSchema.set('toJSON', {
@@ -28,5 +35,26 @@ personSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
+
+// const User = mongoose.model('Person', personSchema);
+// const user = new User();
+// let error;
+
+// user.name = 'Ariel'
+// user.phone = '555.0123';
+// error = user.validateSync();
+// assert.equal(error.errors['phone'].message,
+//   '555.0123 is not a valid phone number!');
+
+// user.phone = '';
+// error = user.validateSync();
+// assert.equal(error.errors['phone'].message,
+//   'User phone number required');
+
+// user.phone = '201-555-0123';
+// // Validation succeeds! Phone number is defined
+// // and fits `DDD-DDD-DDDD`
+// error = user.validateSync();
+// assert.equal(error, null);
 
 module.exports = mongoose.model('Person', personSchema)
